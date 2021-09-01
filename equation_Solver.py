b3469d87cbab5ad1d86347d478b181c3115f5dd7
ndeu <gautamupen99@gmail.com>
2021-09-01 17:18:44 +0545

    Final touch done

--- equation_Solver.py
+++ equation_Solver.py
@@ -1,28 +1,75 @@
+
 print("""***ax^2 + bx + c = 0***""")
 
 coeff_a = float(input("Enter a (i.e coefficient of x^2): "))
 coeff_b = float(input("Enter b (i.e coefficient of x): "))
 coeff_c = float(input("Enter c (i.e constant): "))
 print("""
-
-
       """)
 
-determinant = coeff_b**2 - 4 * coeff_a * coeff_b
+def print_Root(root_1,root_2,coeff_a,coeff_b,coeff_c):
+
+    if coeff_b > 0:
+        print("The roots of the equation;\n"
+        f"{coeff_a} x^2 + {coeff_b} x + {coeff_c} = 0 is:\n\n ")
+
+        print(f"Root 1: {root_1}\nRoot 2: {root_2}")
+
+    else:
+        coeff_b = -coeff_b
+        print("The roots of the equation;\n"
+        f"{coeff_a} x^2 - {coeff_b} x + {coeff_c} = 0 is:\n\n ")
+
+        print(f"Root 1: {root_1}\nRoot 2: {root_2}")
+        
+
+def print_Imaginary(real_Part,imaginary_Part,coeff_a,coeff_b,coeff_c):
+
+    if coeff_b > 0:
+        print("The roots of the equation;\n"
+        f"{coeff_a} x^2 + {coeff_b} x + {coeff_c} = 0 is:\n\n ")
+
+        print(f"Root 1: {real_Part} + i{imaginary_Part}\n")
+        print(f"Root 2: {real_Part} - i{imaginary_Part}\n")
+
+    else:
+        coeff_b = -coeff_b
+        print("The roots of the equation;\n"
+        f"{coeff_a} x^2 + {coeff_b} x + {coeff_c} = 0 is:\n\n ")
+
+        print(f"Root 1: {real_Part} + i {imaginary_Part}\n")
+        print(f"Root 2: {real_Part} - i {imaginary_Part}\n")
+
+
+determinant = coeff_b**2 - 4 * coeff_a * coeff_c
+determinant = round(determinant,3)
+
 
 if determinant > 1:
     determinant = determinant**(1/2)
     root_1 = (-coeff_b + determinant)/ (2 * coeff_a)
+    root_1 = round(root_1,3)
     root_2 = (-coeff_b - determinant)/ (2 * coeff_a)
+    root_2 = round(root_2,3)
+    print_Root(root_1,root_2,coeff_a,coeff_b,coeff_c)
+
 else:
-    if determinant == 0:
+
+    if determinant == 0.00:
         root_1 = root_2 = -coeff_b / (2 * coeff_a)
+        root_1 = round(root_1,3)
+        print_Root(root_1,root_2,coeff_a,coeff_b,coeff_c)
+
     else:
         determinant = (-determinant)**(1/2)
-        root_1 = str(-coeff_b / (2 * coeff_a)) + " i" + str(determinant / (2 * coeff_a))
-        root_2 = str(-coeff_b / (2 * coeff_a)) + " i" + str(determinant / (2 * coeff_a))
+        real_Part = (-coeff_b) / (2 * coeff_a)
+        imaginary_Part = determinant / (2 * coeff_a)
+        real_Part = round(real_Part,3)
+        imaginary_Part = round(imaginary_Part,3)
+        print_Imaginary(real_Part,imaginary_Part,coeff_a,coeff_b,coeff_c)
 
 
-print(f"The roots of the equation {coeff_a}x^2 + {coeff_b}x + c are:")
-print(f"Root 1: {root_1} and Root 2: {root_2}")
+
+
+
 
