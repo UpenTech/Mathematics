b1e555e2d91e4d6a775c64b9a89d1966684d9d31
ndeu <gautamupen99@gmail.com>
2021-09-01 16:27:33 +0545

    Input taken

---
+++ equation_Solver.py
@@ -0,0 +1,12 @@
+print("""***ax^2 + bx + c = 0***""")
+
+coeff_a = float(input("Enter a (i.e coefficient of x^2): "))
+coeff_b = float(input("Enter b (i.e coefficient of x): "))
+coeff_c = float(input("Enter c (i.e constant): "))
+
+
+determinant = coeff_b**2 - 4 * coeff_a * coeff_b
+
+print(determinant);
+
+
